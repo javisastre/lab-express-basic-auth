5ed8dd06cd5e3ace7cd0d3f54ce42ec638f1e8ab
Javi Sastre <javisastrecal@gmail.com>
2021-02-09 18:15:07 +0100

    done

--- app.js
+++ app.js
@@ -1,37 +1,56 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const cookieParser = require('cookie-parser');
-const express = require('express');
-const favicon = require('serve-favicon');
-const hbs = require('hbs');
-const mongoose = require('mongoose');
-const logger = require('morgan');
-const path = require('path');
+const cookieParser = require("cookie-parser");
+const express = require("express");
+const favicon = require("serve-favicon");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const path = require("path");
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+
+const app_name = require("./package.json").name;
+const debug = require("debug")(
+  `${app_name}:${path.basename(__filename).split(".")[0]}`
+);
 
 const app = express();
 
 // require database configuration
-require('./configs/db.config');
+require("./configs/db.config");
 
 // Middleware Setup
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+
+app.use(
+  session({
+    secret: "randomExcercise", // this should be in the env file as SESSION_SECRET=randomasdfgh and accessed as process.env.SESSION_SECRET
+    cookie: { maxAge: 3600000 * 3 }, // 3 hours alive
+    resave: true, // refresh the session
+    saveUninitialized: false,
+    store: new MongoStore({
+      //this will create a new collection
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60 * 24 * 7, //time to live in the database (14 days - Default)
+    }),
+  })
+);
 
 // default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
+app.locals.title = "Express - Generated with IronGenerator";
 
-const index = require('./routes/index.routes');
-app.use('/', index);
+const index = require("./routes/index.routes");
+app.use("/", index);
 
 module.exports = app;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,98 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express");
+const indexRouter = express.Router();
+const User = require("./../models/User.model");
+
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+indexRouter.get("/", (req, res, next) => res.render("index"));
 
-module.exports = router;
+indexRouter.get("/signup", (req, res, next) => res.render("signup"));
+
+indexRouter.post("/signup", (req, res, next) => {
+  const { username, password } = req.body;
+  if (username === "" || password === "") {
+    res.render("/signup", { errorMessage: "Username and password are needed" });
+    return;
+  }
+
+  // check if username is taken
+  User.findOne({ username: username }).then((user) => {
+    // if username is taken, display error message
+    if (user !== null) {
+      res.render("auth-views/signup-form", {
+        errorMessage: "Provide correct data",
+      });
+      return; // we terminate the process
+    }
+
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    User.create({ username: username, password: hashedPassword })
+      .then((user) => {
+        req.session.currentUser = user;
+        res.redirect("/main");
+      })
+      .catch((err) => {
+        res.render("signup", {
+          errorMessage: "Username and password are needed",
+        });
+      });
+  });
+});
+
+indexRouter.get("/login", (req, res, next) => res.render("login"));
+
+indexRouter.post("/login", (req, res, next) => {
+  const { username, password } = req.body;
+  if (username === "" || password === "") {
+    res.render("/login", { errorMessage: "Username and password are needed" });
+    return;
+  }
+
+  User.findOne({ username })
+    .then((user) => {
+      if (!user) {
+        res.render("/login", { errorMessage: "Enter a valid username" });
+        return;
+      }
+
+      const passwordCorrect = bcrypt.compareSync(password, user.password);
+
+      if (passwordCorrect) {
+        req.session.currentUser = user;
+        res.redirect("/main");
+      } else {
+        res.render("/login", {
+          errorMessage: "Indicate username and password",
+        });
+      }
+    })
+    .catch((err) => {
+      res.redirect("/login");
+    });
+});
+
+indexRouter.get("/main", (req, res, next) => {
+  if (req.session.currentUser) {
+    const data = {
+      username: req.session.currentUser.username,
+      password: req.session.currentUser.password,
+    };
+    res.render("main", data);
+  } else {
+    res.redirect("/");
+  }
+});
+
+indexRouter.get("/private", (req, res, next) => {
+  if (req.session.currentUser) {
+    res.render("private");
+  } else {
+    res.redirect("/");
+  }
+});
+
+module.exports = indexRouter;
